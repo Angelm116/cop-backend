358b827264faa0afcc465f78138881d2f602988b
Angelm116 <44384080+Angelm116@users.noreply.github.com>
2021-04-18 20:30:27 -0400

    fix 4

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const bodyParser = require('body-parser');
 var express = require('express');
 var routes = require('./routes/routes.js');
-
 var app = express();
+var cors = require('cors');
+
+// use it before all route definitions
+app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
+app.use(routes);
+
 
 var port = 3000;
 
