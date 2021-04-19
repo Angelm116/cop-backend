5b87b7de39ccc80c00ac4eb037331c39959f0842
Angelm116 <44384080+Angelm116@users.noreply.github.com>
2021-04-19 17:36:05 -0400

    worked on events

--- models/eventModel.js
+++ models/eventModel.js
@@ -19,83 +19,68 @@ Event.create_event = (new_event, resultF) => {
   var new_date = new Date('1970-01-01T' + new_event.event_date + 'Z');
   var date_holder = undefined;
 
-  (events)=>{return new Promise((resolve, reject)=>{
-    dbConnection.query(`SELECT event_id, event_date FROM Event WHERE location_id = '${new_event.location_id}' AND event_date = '${new_event.event_date}'`, (err, res) => {
+  
+   dbConnection.query(`SELECT event_id, event_date FROM Event WHERE location_id = '${new_event.location_id}' AND event_date = '${new_event.event_date}'`, (err, res) => {
 
-      if (err)
-      {
-        reject(err);
-      }
-  
-      resolve(res);
-      // events = res;
-      // console.log(events);
-  
-      
-    })(events).then(events => {if(events)
+    if (err)
     {
-      console.log(events);
-      for(x in events){
-        date_holder = new Date('1970-01-01T' + x.event_date + 'Z');
-  
-        if ( 
-          
-          (new_date.getHours() < date_holder.getHours()) && (date_holder.getHours() < (new_date.getHours() + 1))
-          ||
-          (new_date.getHours() < (date_holder.getHours() + 1)) && ((date_holder.getHours() + 1) < (new_date.getHours() + 1))
+      resultF(err, null);
+    }
+
+    if (res)
+    {
+      events = JSON.parse(res);
+    }
+    // events = res;
+    // console.log(events);
+
+    if(events)
+  {
+    console.log(events);
+    for(x in events){
+      date_holder = new Date('1970-01-01T' + x.event_date + 'Z');
+
+      if ( 
         
+        (new_date.getHours() < date_holder.getHours()) && (date_holder.getHours() < (new_date.getHours() + 1))
+        ||
+        (new_date.getHours() < (date_holder.getHours() + 1)) && ((date_holder.getHours() + 1) < (new_date.getHours() + 1))
+      
+      )
+      {
+        if
+         (
+          (new_date.getMinutes() < date_holder.getMinutes()) && (date_holder.getMinutes() < (new_date.getMinutes() + 1))
+          ||
+          (new_date.getMinutes() < (date_holder.getMinutes() + 1)) && ((date_holder.getMinutes() + 1) < (new_date.getMinutes() + 1))
         )
         {
           if
-           (
-            (new_date.getMinutes() < date_holder.getMinutes()) && (date_holder.getMinutes() < (new_date.getMinutes() + 1))
-            ||
-            (new_date.getMinutes() < (date_holder.getMinutes() + 1)) && ((date_holder.getMinutes() + 1) < (new_date.getMinutes() + 1))
-          )
-          {
-            if
-           (
-            (new_date.getSeconds() < date_holder.getSeconds()) && (date_holder.getSeconds() < (new_date.getSeconds() + 1))
-            ||
-            (new_date.getSeconds() < (date_holder.getSeconds() + 1)) && ((date_holder.getSeconds() + 1) < (new_date.getSeconds() + 1))
-          )
-          {
-            resultF(null, {error:"the event time coincides with anoter event"});
-          }
-          }
+         (
+          (new_date.getSeconds() < date_holder.getSeconds()) && (date_holder.getSeconds() < (new_date.getSeconds() + 1))
+          ||
+          (new_date.getSeconds() < (date_holder.getSeconds() + 1)) && ((date_holder.getSeconds() + 1) < (new_date.getSeconds() + 1))
+        )
+        {
+          resultF(null, {error:"the event time coincides with anoter event"});
+        }
         }
       }
     }
-  
-    dbConnection.query(`INSERT INTO Event SET ?`, new_event, (err, res) => {
-  
-      if(err){
-        resultF(err, null);
-        return;
-    }
-  
-    resultF(null, {result : "success"});
-  
-    });})
-  
-  })}
+  }
 
-  //  dbConnection.query(`SELECT event_id, event_date FROM Event WHERE location_id = '${new_event.location_id}' AND event_date = '${new_event.event_date}'`, (err, res) => {
+  dbConnection.query(`INSERT INTO Event SET ?`, new_event, (err, res) => {
 
-  //   if (err)
-  //   {
-  //     resultF(err, null);
-  //   }
+    if(err){
+      resultF(err, null);
+      return;
+  }
 
-  //   if (res)
-  //   {
-  //     events = JSON.parse(res);
-  //   }
-  //   // events = res;
-  //   // console.log(events);
+  resultF(null, {result : "success"});
 
+  });
     
-  // })
+  })
 
 
   
