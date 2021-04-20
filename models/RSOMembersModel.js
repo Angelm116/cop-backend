e9cb1e8021510cfb0afbb5e8bee285d9d9db3ca5
Angelm116 <44384080+Angelm116@users.noreply.github.com>
2021-04-19 20:07:47 -0400

    worked on events

--- models/RSOMembersModel.js
+++ models/RSOMembersModel.js
@@ -1,8 +1,7 @@
 var dbConnection = require('../dbConnection');
 
 const RSOMember = function(rsoMember)
-{
-  this.rso_memberid = rsoMember.rso_memberid, 
+{ 
   this.user_id = rsoMember.user_id, 
   this.rso_id = rsoMember.rso_id
 }
